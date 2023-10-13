5d8e62c205f2e4e547fa55870579841002474246
siskoweb <sisko4dev@gmail.com>
2023-10-13 10:16:28 +0100

    debugging

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ const toggleButton = document.getElementById("toggle");
 const linksNav = document.getElementById("links");
 
 toggleButton.addEventListener("click", function () {
+    //if navbar appear hide it if not display it
     if (linksNav.classList.contains("show")) {
         linksNav.classList.remove("show");
     } else {
